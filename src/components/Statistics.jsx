e0dfc0a8668c31be6b9c9715069b0fd524f81055
georgiana <gchiriac2012@gmail.com>
2025-03-01 15:52:54 -0500

    Exercise 1.9

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,19 +1,39 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const Statistics = ({ good, neutral, bad }) => {
+  const [feedback, setFeedback] = useState(false);
+
+  /* Statistics:
+  - good votes count as +1
+  - bad votes count as -1
+  neutral votes are 0 and don't affect the numerator
+  */
   const all = good + neutral + bad;
   const average = (good * 1 + bad * -1) / (good + neutral + bad);
   const positive = `${(good / (good + neutral + bad)) * 100} %`;
 
+  useEffect(() => {
+    if (good > 0 || neutral > 0 || bad > 0) {
+      setFeedback(true);
+    }
+  }, [good, neutral, bad]);
+
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      {feedback ? (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive}</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
